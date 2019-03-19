f6a5213cd41a26b49cb41ad1c3c75db27b334498
Tim Molderez <id@timmolderez.be>
2019-03-19 19:09:52 +0100

    Refactored Location to use int[] instead of ArrayList<Integer>

--- src/be/intimals/freqt/structure/Location.java
+++ src/be/intimals/freqt/structure/Location.java
@@ -5,15 +5,19 @@ import java.util.*;
 public class Location {
     private int id;
     //private int pos; //right most position
-    private List<Integer> pos = new ArrayList<>(); //positions of all labels in pattern
+//    private List<Integer> pos = new ArrayList<>();
 
+    private int[] pos; //positions of all labels in pattern
 
     //private Set<Integer> rootPos = new LinkedHashSet<>();
     ////////////////////////////////////////////////////////////
 
-    public Location(){}
+    public Location(){
+        pos = new int[0];
+    }
     public Location(List<Integer> start) {
-        pos = new ArrayList<>(start);
+        pos = toPrimitiveIntArray(start);
+//        pos = new ArrayList<>(start);
     }
 
     public void setLocationId(int a) {
@@ -23,15 +27,47 @@ public class Location {
         return this.id;
     }
 
-    public void addLocationPos(int a){
-        this.pos.add(a);
+    public void addLocationPos(int a) {
+        pos = appendArray(pos, a);
+//        this.pos.add(a);
     }
+
     public int getLocationPos() {
-        return this.pos.get(this.pos.size()-1);
+        return pos[pos.length-1];
+//        return this.pos.get(this.pos.size()-1);
     }
+
     public List<Integer> getLocationList() {
-        return this.pos;
+        return toArrayList(pos);
     }
 
+    // Convert an int[] to a List<Integer>
+    private int[] toPrimitiveIntArray(List<Integer> al) {
+        int[] converted = new int[al.size()];
+        Iterator<Integer> iterator = al.iterator();
+        for (int i = 0; i < converted.length; i++)
+        {
+            converted[i] = iterator.next().intValue();
+        }
+        return converted;
+    }
 
+    // Convert a List<Integer> to an int[]
+    private List<Integer> toArrayList(int[] arr) {
+        List<Integer> converted = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++) {
+            converted.add(arr[i]);
+        }
+        return converted;
+    }
+
+    // Append an integer to an array
+    private int[] appendArray(int[] array, int x){
+        int[] result = new int[array.length + 1];
+        for(int i = 0; i < array.length; i++)
+            result[i] = array[i];
+
+        result[result.length - 1] = x;
+        return result;
+    }
 }
