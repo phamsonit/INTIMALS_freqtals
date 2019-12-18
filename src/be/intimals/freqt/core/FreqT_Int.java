a95c8039be2a6dea71abec31c162c172e0ac2bae
Johan Fabry <johan@raincode.com>
2019-12-18 14:45:13 +0100

    [Perf] Add an opportunistic implementation of the fast subtree check algorithm

--- src/be/intimals/freqt/core/FreqT_Int.java
+++ src/be/intimals/freqt/core/FreqT_Int.java
@@ -122,28 +122,156 @@ public class FreqT_Int {
      * @return
      */
     private int checkSubTree(ArrayList<Integer> pat1, ArrayList<Integer> pat2) {
+
+        int fastResult = fastCheckSubTree(pat1,pat2);
+        if (fastResult != -1) return fastResult;
+
         //check subset of labels before check maximality
         if (checkSubsetLabel(pat1, pat2)) {
             //maximality check
             FreqT_Int_subtree fr = new FreqT_Int_subtree(this.config);
-            int pat1Size = Pattern_Int.countNode(pat1);
-            int pat2Size = Pattern_Int.countNode(pat2);
+            int pat1Size = pat1.size();
+            int pat2Size = pat2.size();
             if(pat1Size < pat2Size)
                 fr.checkSubtrees(pat1,pat2);
             else
                 fr.checkSubtrees(pat2,pat1);
             if (fr.getOutputPattern() == null) {
+    //                if (fastResult != 0 && fastResult != -1)
+    //                    fastCheckSubTree(pat1,pat2); //for debugging: put a breakpoint here
                 return 0; //not related
             } else {
                 if (pat1Size <= pat2Size) {
+    //                    if (fastResult != 1 && fastResult != -1)
+    //                        fastCheckSubTree(pat1,pat2); //for debugging: put a breakpoint here
                     return 1; //pat1 is a subtree of pat2
                 } else {
+    //                    if (fastResult != 2 && fastResult != -1)
+    //                        fastCheckSubTree(pat1,pat2); //for debugging: put a breakpoint here
                     return 2; //pat2 is a subtree of pat1
                 }
             }
         }else {
+    //        if (fastResult != 0)
+    //            fastCheckSubTree(pat1,pat2);  //for debugging: put a breakpoint here
             return 0;
         }
+    }
+
+    public int hitCount,missCount;
+
+    // 0 = no subtree
+    // 1 = pat1 is a subtree of pat2
+    // 2 = pat2 is a subtree of pat1
+    private int fastCheckSubTree(ArrayList<Integer> pat1, ArrayList<Integer> pat2){
+        hitCount++;
+        if(pat1.size() == pat2.size())
+        {
+            if (pat1.equals(pat2)) return 1; else return 0;
+        }
+        try {
+            if (pat1.size() > pat2.size()) {
+                if (hasSubtree(pat1, pat2))
+                    return 2;
+                else
+                    return 0;
+            }
+
+            //pat2.size() > pat1.size();
+            if (hasSubtree(pat2, pat1))
+                return 1;
+            else
+                return 0;
+        }
+        catch (IndexOutOfBoundsException ex){
+            hitCount--;
+            missCount++;
+            return -1;
+        }
+    }
+
+    protected boolean hasSubtree(List<Integer> big, List<Integer> small){
+        int root = small.get(0); //the root of small
+        int smallSize = small.size();
+        int bigSize = big.size();
+        int startIdx = 0;
+
+        List<Integer> bigPart = big;
+        while(true) //loop over big, searching for the root
+        {
+            int rootIdx = bigPart.indexOf(root);
+            if (rootIdx == -1)
+                return false;
+            int bigPartSize =  bigPart.size();
+            if (rootIdx + smallSize > bigPartSize)
+                return false;
+            if(treeIncludes(bigPart.subList(rootIdx, bigPartSize),small))
+                return true;
+            startIdx += rootIdx+1;
+            bigPart = big.subList(startIdx,bigSize); //continue with the rest of the array
+        }
+    }
+
+    //both big and small have the same root
+    //inclusion check ignores sub-trees that are in big but not in small
+    private boolean treeIncludes(List<Integer> big, List<Integer> small){
+        if (big.size() == small.size()) return big.equals(small);
+
+        int smallSize = small.size();
+        int bigSize = big.size();
+        int smallIndex = 1;
+        int bigIndex = 1;
+
+        while( smallIndex < smallSize) { //loop until the end of the small tree
+            if(bigIndex >= bigSize)
+                return false; //there is more in small that is not in big
+            int bigNode = big.get(bigIndex);
+            int smallNode = small.get(smallIndex);
+
+            while (bigNode != smallNode) {
+                if (bigNode < -1) {
+                    bigIndex += 2; //skip over leaves in big but not in small
+                    if (bigIndex >= bigSize)
+                        return false; //there is more in small that is not in big
+                }
+                //in a branch in big that has the same prefix but continues differently in small
+                //we need to go back and skip over it -- complex case
+                else if (bigNode == -1) {
+                    throw new IndexOutOfBoundsException();
+                }
+                //in big we have a branch that is not in small, skip over it
+                else {
+                    bigIndex = skipOver(big, bigIndex + 1);
+                    if (bigIndex >= bigSize)
+                        return false; //there is more in small that is not in big
+                }
+                bigNode = big.get(bigIndex);
+            }
+
+            bigIndex++;smallIndex++;
+        }
+
+        return true;
+    }
+
+    // in the tree at offset-1 there is the start of a subtree that we should skip over
+    // return the offset in the tree after that subtree
+    private int skipOver(List<Integer> tree, int offset){
+        offset++;
+        int treeSize = tree.size();
+        int recursion = 1; //how deep are we recursing in the subtree
+        while(recursion >= 0){
+            if(offset >= treeSize)
+                return offset; //end of the big tree, break out
+            int node = tree.get(offset);
+            if (node == -1 )
+                recursion--;
+            else
+                recursion++;
+
+            offset++;
+        }
+        return offset;
     }
 
     //check output patterns by using minLeaf and minNode
@@ -815,6 +943,8 @@ public class FreqT_Int {
                 //parallel running
                 //FreqT_Int_ext_multi freqT_ext = new FreqT_Int_ext_multi(config, this.grammar, this.grammarInt, this.blackLabelsInt, this.whiteLabelsInt,this.xmlCharacters,this.labelIndex,this.transaction);
                 freqT_ext.run(rootIDs, start, report);
+                //for FastTreeCheck
+                System.out.println("fastCheckSubTree Hits: "+ freqT_ext.hitCount+" Misses: "+ freqT_ext.missCount);
 
             }else{//output maximal patterns in the first step
                 log(report,"OUTPUT");
---
+++ src/be/intimals/freqt/core/HasSubtreeTest.java
@@ -0,0 +1,123 @@
+package be.intimals.freqt.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HasSubtreeTest {
+
+    @Test
+    public void testSubtreePresent(){
+        Integer[] big =   {51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, 67, 55, 56, 65, 54, 65, 4, 7, 8, -93, -1, -1, -1, -1, -1, -1, 4, 7, 8, -152};
+        Integer[] small = {51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, 67, 55, 56, 65, 54, 65, 4, 7, 8, -93};
+
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertTrue(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+
+    @Test
+    public void testSubtreeAbsent1(){
+        Integer[] big = {51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, 67, 55, 56, 65, 54, 65, 4, 7, 8, -93, -1, -1, -1, -1, -1, -1, 4, 7, 8, -152};
+        Integer[] small = {51, 52, 201, 69, 39, 4, 7, 8, -26, -1, -1, -1, -1, -1, -1, 101, 102, 4, 7, 8, -132, -1, -1, -1, -1, 103, 65, 4, 7, 8, -93};
+
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertFalse(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+
+    @Test
+    public void testSubtreeAbsent2(){
+        Integer[] big = {51, 52, 201, 69, 39, 4, 7, 8, -26, -1, -1, -1, -1, -1, -1, 101, 102, 4, 7, 8, -132, -1, -1, -1, -1, 103, 65, 4, 7, 8, -93};
+        Integer[] small = {51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, 67, 55, 56, 65, 54, 65, 4, 7, 8, -93, -1, -1, -1, -1, -1, -1, 4, 7, 8, -152};
+
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertFalse(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+
+    @Test
+    public void testSubtreeAbsent3(){
+        Integer[] big = {51, 52, 64, 54, 65, 4, 7, 8, -89, -1, -1, -1, -1, 67, 7, 8, -91, -1, -1, -1, -1, -1, -1, -1, 201, 69, 39, 4, 7, 8, -90, -1, -1, -1, -1, -1, -1, 101, 102, 4, 7, 8, -91, -1, -1, -1, -1, 103, 68, 69, 39, 4, 7, 8, -278, -1, -1, -1, -1, -1, -1, 279, 280, 41, 42, 4, 7, 8, -281, -1, -1, -1, -1, 50, 51, 52, 64, 54, 282, 4, 7, 8, -281};
+        Integer[] small = {51, 52, 238, 239, 78, 69, 39, 4, 7, 8, -171, -1, -1, -1, -1, -1, -1, 79, -11, -1, -1, 4, 7, 8, -240};
+
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertFalse(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+
+    @Test
+    public void testSubtreeAbsent4(){
+        Integer[] big = {51, 52, 64, 54, 65, 4, 7, 8, -89, -1, -1, -1, -1, -1, -1, -1, 238, 239, 78, 69, 39, 4, 7, 8, -171, -1, -1, -1, -1, -1, -1, 79, -11};
+        Integer[] small = {51, 52, 238, 239, 78, 69, 39, 4, 7, 8, -171, -1, -1, -1, -1, -1, -1, 79, -11, -1, -1, 4, 7, 8, -240};
+
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertFalse(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+
+    @Test
+    public void testSkipoverBreak(){
+        Integer[] big = {51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, 67, 55, 56, 65, 54, 65, 4, 7, 8, -93, -1, -1, -1, -1, -1, -1, 4, 7, 8, -152};
+        Integer[] small = {51, 52, 201, 69, 39, 4, 7, 8, -26, -1, -1, -1, -1, -1, -1, 101, 102, 4, 7, 8, -132, -1, -1, -1, -1, 103, 65, 4, 7, 8, -93};
+
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertFalse(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+
+    @Test
+    public void testSkipoverEnds(){
+        Integer[] big =   {51, 52, 64, 54, 65, 4, 7, 8, -192, -1, -1, -1, -1, -1, -1, -1, 64, 54, 65, 4, 7, 8, -283, -1, -1, -1, -1, 67, 65, 54, 7, 8, -284, -1, -1, -1, -1, 4, 7, 8, -205};
+        Integer[] small = {51, 52, 201, 69, 39, 4, 7, 8, -26, -1, -1, -1, -1, -1, -1, 101, 102, 4, 7, 8, -132, -1, -1, -1, -1, 103, 65, 4, 7, 8, -93};
+
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertFalse(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+
+    /* Omitting these cases for now
+    @Test
+    public void testMismatchedLeaf(){
+        Integer[] big =   {271, 272, 273, 272, 277, 279, 280, 281, 286, 287, 112, 63, -123, -1, -1, -1, -1, 288, 136, 137, 138, 28, 49, 10, -104, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 277, 279, 280, 281, 380, 288, 314, 315, 136, 137, 138, 28, 49, 10, -104, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 287, 381, 304, 80, 10, -102, -1, -1, -1, 80, 10, -346, -1, -1, -1, 80, 10, -929, -1, -1, -1, -1, -1, -1, -1, 324, 325, 326, 327, 136, 137, 138, 28, 49, 10};
+        Integer[] small = {271, 272, 273, 272, 277, 279, 280, 281, 286, 287, 112, 63, -123, -1, -1, -1, -1, 288, 136, 137, 138, 28, 49, 10, -104, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 277, 279, 280, 281, 380, 288, 314, 315, 136, 137, 138, 28, 49, 10, -104, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 287, 381, 304, 80, 10, -102, -1, -1, -1, 80, 10, -1993, -1, -1, -1, 80, 10, -1994};
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertFalse(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+*/
+    /* Omitting these cases for now
+    @Test
+    public void testBacktracking(){
+        Integer[] big =   { 51, 52, 64, 54, 65, 4, 7, 8, -192, -1, -1, -1, -1, -1, -1, -1, 64, 54, 65, 4, 7, 8, -204, -1, -1, -1, -1, -1, -1, -1, 64, 54, 65, 54, 7, 8, -285, -1, -1, -1, -1, 4, 7, 8, -224, -1, -1, -1, -1, 67, 65, 4, 7, 8, -291 };
+        Integer[] small = { 51, 52, 64, 54, 65, 4, 7, 8, -192, -1, -1, -1, -1, -1, -1, -1,                                                        64, 54, 65, 54, 7, 8, -285, -1, -1, -1, -1, 4, 7, 8, -224, -1, -1, -1, -1, 67, 65, 4, 7, 8, -291 };
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertTrue(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+    */
+
+    @Test
+    public void testSkip2Subtrees(){
+        Integer[] big =   { 51, 52, 53, 54, 55, 56, 65, 4, 7, 8, -93, -1, -1, -1, -1, -1, -1, 58, -59, -1, -1, 60, 61, -62, -1, -1, -1, -1, -1, 63, 51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 87, 51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, 67, 124, 125, -11 };
+        Integer[] small = { 51, 52, 53, 54, 55, 56, 65, 4, 7, 8, -93, -1, -1, -1, -1, -1, -1,                                           -1, -1, 63, 51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 87, 51, 52, 64, 54, 65, 4, 7, 8, -96, -1, -1, -1, -1, 67, 124, 125, -11 };
+        FreqT_Int fti = new FreqT_Int(null);
+
+        assertTrue(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+
+    /* Omitting these cases for now
+    @Test
+    public void testBacktrackingBifurcated(){
+        Integer[] big =   { 51, 52, 64, 54, 65, 4, 7, 8, -89, -1, -1, -1, -1, 67, 7, 8, -91, -1, -1, -1, -1, -1, -1, -1, 201, 69, 39, 4, 7, 8, -90, -1, -1, -1, -1, -1, -1, 101, 102, 4, 7, 8, -91, -1, -1, -1, -1, 103, 68, 69, 39, 4, 7, 8, -278, -1, -1, -1, -1, -1, -1, 279, 280, 41, 42, 4, 7, 8, -25, -1, -1, -1, -1, 50, 51, 52, 64, 54, 282, 4, 7, 8, -25, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 42, 4, 7, 8, -286 };
+        Integer[] small = { 51, 52, 64, 54, 65, 4, 7, 8, -89, -1, -1, -1, -1, 67, 7, 8, -91, -1, -1, -1, -1, -1, -1, -1, 201, 69, 39, 4, 7, 8, -90, -1, -1, -1, -1, -1, -1, 101, 102, 4, 7, 8, -91, -1, -1, -1, -1, 103, 68, 69, 39, 4, 7, 8, -278, -1, -1, -1, -1, -1, -1, 279, 280, 41, 42, 4, 7, 8, -286 };
+        FreqT_Int fti = new FreqT_Int(null);
+
+        //is true but too complex to handle
+        assertFalse(fti.hasSubtree(Arrays.asList(big),Arrays.asList(small)));
+    }
+*/
+}
